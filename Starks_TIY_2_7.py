4110f6f0296907b4cb8d039348beb7d63d6f4e83
Julian <julian-jordan.starks@fwcsstudents.org>
2024-08-30 13:16:05 -0400

    Add files via upload

---
+++ Starks_TIY_2_7.py
@@ -0,0 +1,14 @@
+name = "Joe"
+print(f"Your name is:\n\t{name}")
+
+name = "Joe "
+name = name.rstrip()
+print(f"Your name is:\n\t{name}")
+
+name = " Joe"
+name = name.lstrip()
+print(f"Your name is:\n\t{name}")
+
+name = " Joe "
+name = name.strip()
+print(f"Your name is:\n\t{name}")
